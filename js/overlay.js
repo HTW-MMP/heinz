67d1a1835ad21abbfebe1ef6cc488a2ee0b7478d
myriam <myriam@halfapx.com>
2017-10-03 23:16:04 +0200

    more overlay

--- js/overlay.js
+++ js/overlay.js
@@ -1,9 +1,22 @@
 $('.toggle').click(function(){
     var ol = $(this).data('toggle-overlay');
     $('#'+ol).toggleClass('open');
-    $('body').toggleClass('overlay-open');
+    $('body').toggleClass('overlay-open');$
 });
+
+// close on X
 $('.overlay .close').click(function(){
-    $(this).closest('.overlay').removeClass('open');
-    $('body').removeClass('overlay-open');
+    closeOverlay();
 });
+
+// close on esc
+$(document).keyup(function(e) {
+  if (e.keyCode === 27){
+      closeOverlay();
+  }
+});
+
+function closeOverlay(){
+    $('.overlay').removeClass('open');
+    $('body').removeClass('overlay-open');
+}
