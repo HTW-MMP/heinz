a397a3a85bc6ca9d96becdb6581abeae444c9265
myriam <myriam@halfapx.com>
2017-11-20 19:25:25 +0100

    docs

--- js/heinz.js
+++ js/heinz.js
@@ -3,21 +3,20 @@ $(document).ready( function(){
     var toc = $('.toc');
     var headings = [];
     $('h1, h2, h3').each(function(){
-        var h = $(this).text(),
-            id = null;
+        if($(this).parent().is('.toc') || $(this).hasClass('no-toc')){
+            // these are the headings excluded
+        } else {
+            var h = $(this).text(),
+                id = null;
 
-        if($(this).attr('id')){
-           id = $(this).attr('id');
-        } else{
-            id = h.replace(/ /g,'')
-            $(this).attr('id', id);
-        }
-        var l = '<a href="#'+id+'" class="'+$(this).prop("tagName")+'">'+h+'</a>';
-
-        if($(this).parent().is('#toc') || $(this).hasClass('no-toc')){
-            console.log($(this).text());
-        } else{
-            headings.push(l);
+            if($(this).attr('id')){
+               id = $(this).attr('id');
+            } else{
+                id = h.replace(/ /g,'')
+                $(this).attr('id', id);
+            }
+            var l = '<a href="#'+id+'" class="'+$(this).prop("tagName")+'">'+h+'</a>';
+                headings.push(l);
         }
     });
 
@@ -33,7 +32,7 @@ $(document).ready( function(){
     $(".table-of-contents a").click(function() {
 		var id = $(this).attr('href');
 		$('html, body').animate({
-			scrollTop: $(id).offset().top - navheight
+			scrollTop: $(id).offset().top - navheight - 30
 		}, 1000);
 	});
 });
