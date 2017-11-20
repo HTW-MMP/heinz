854a7827e808bd85fc27424c08eff9fc5c27774f
myriam <myriam@halfapx.com>
2017-11-20 18:55:34 +0100

    code visibility + copy to clipboard

--- js/heinz.js
+++ js/heinz.js
@@ -88,6 +88,22 @@ function closeOverlay(){
 
 $(document).ready(function(){
     $('.menu .toc ul').unwrap();
+
+    $('pre code').before('<span class="copy-code">Copy To Clipboard</span>');
+
+    $('.copy-code').click(function() {
+        var temp = $("<input>");
+        $("body").append(temp);
+        temp.val($(this).next('code').text()).select();
+        document.execCommand("copy");
+        temp.remove();
+        alert('Code was copied to clipboard');
+    });
+
+    $('.toggle-code').click(function() {
+        $(this).toggleClass('off');
+        $('pre').toggle();
+    });
 });
 
 // Slider 
