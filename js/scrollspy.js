e1fa1565035f6cd4f869ea47e5e1720affa0cb59
Pascal Lüthi <pascalluethi.bfh@gmail.com>
2017-11-20 10:38:29 +0100

    scrollspy

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,35 +1,44 @@
 // scrollspy
 var bodyheight = $(document).outerHeight(),
     navheight = $('nav.menu').outerHeight();
-
+	
 $('body').css('margin-top', navheight);
 
-$("section").each(function(i) {
-    var offset = $(this).offset().top,
-        height = $(this).outerHeight(),
-        p = (offset / bodyheight) * 100,
-        w = (height / bodyheight) * 100;
+$(document).ready(function() {
+	bodyheight = $(document).outerHeight() - $(window).innerHeight();
 
-    if ($(this).attr('data-section-title')) {
-        var title = $(this).data('section-title');
-    } else {
-        var title = $(this).children(':header').first().text();
-    }
+	$("section").each(function(i) {
+		var offset = $(this).offset().top,
+			height = $(this).outerHeight(),
+			p = (offset / bodyheight) * 100,
+			w = (height / bodyheight) * 100;
+		if ($(this).attr('data-section-title')) {
+			var title = $(this).data('section-title');
+		} else {
+			var title = $(this).children(':header').first().text();
+		}
 
-    $(this).attr('id', 'section-'+i);
+		$(this).attr('id', 'section-'+i);
+		
+		console.log("Section ID: " + i + " width: " + w + " left: " + p);
 
-    $(".scrollspy-wrapper").append('<div data-go-to="section-' + i + '" class="scrollspy" style="width:'+w+'%; left:'+p+'%">' + title + '</div>');
+		$(".scrollspy-wrapper").append('<div data-go-to="section-' + i + '" class="scrollspy" style="width:'+w+'%; left:'+p+'%">' + title + '</div>');
+	});
+	
+	$(".scrollspy").click(function() {
+		var id = $(this).data('go-to');
+		$('html, body').animate({
+			scrollTop: $('#'+id).offset().top - navheight
+		}, 1000);
+	});
 });
 
 $(document).scroll(function() {
     var scrollTop = $(document).scrollTop(),
-        currentTop = ((scrollTop + $(window).outerHeight()) / bodyheight) * 100;
+		bodyheight = $(document).outerHeight() - $(window).innerHeight(),
+        currentTop = ((scrollTop) / (bodyheight)) * 100;
+				
+		console.log("currentTop ( (scrollTop + window.innerHeight) / bodyheight ) * 100: " + currentTop);
+		
     $(".scrollbar").css("width", currentTop+'%');
-});
-
-$(".scrollspy").click(function() {
-    var id = $(this).data('go-to');
-    $('html, body').animate({
-        scrollTop: $('#'+id).offset().top - navheight
-    }, 1000);
 });
